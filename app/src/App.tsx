d97df5db1abcc7d7464eaa71f98cc05c5350b3bd
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-03-15 17:04:14 +0100

    chore: converted app build set-up from CRA to vite

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      HELLO WORLD
+    </div>
+  )
+}
+
+export default App
