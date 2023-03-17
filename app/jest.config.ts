f44093f2a0fe557790b4c0f720a2d420a04bf5ca
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-03-17 17:51:30 +0100

    progress: wrote tests for HomeScreen component

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -1,11 +1,16 @@
 const config = {
     testEnvironment: "jsdom",
     transform: {
-        "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.(js|jsx)$": "babel-jest",
+        "^.+\\.css$": "jest-transform-css"
     },
     moduleNameMapper: {
         "\\.(css|less|scss)$": "identity-obj-proxy"
     },
+    moduleDirectories: [
+        'node_modules',
+    ],
     setupFilesAfterEnv: [
         "<rootDir>/src/jest.setup.ts"
     ]
--- app/src/__test__/HomeScreen.test.tsx
+++ app/src/__test__/HomeScreen.test.tsx
@@ -1,9 +1,71 @@
-import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import { MemoryRouter, Routes } from 'react-router-dom';
 import HomeScreen from "../screens/HomeScreen"
+import GetRoutes from '../utils/routes';
 
-test("render home page correctly", () => {
-    render(<MemoryRouter><HomeScreen /></MemoryRouter>)
 
-    expect(true).toBeTruthy()
+jest.mock("nanoid", () => {
+    return { nanoid: () => Math.random() * 73647346374 };
+});
+
+
+
+
+describe("Unit Tests", () => {
+
+    it("input field should update as a user types", async () => {
+        const user = userEvent.setup()
+        render(<MemoryRouter><HomeScreen /></MemoryRouter>)
+
+        const string = "aabcaccda"
+
+        const inputField = screen.getByTestId("inputField") as HTMLInputElement;
+        await user.type(inputField, string);
+
+        expect(inputField.value).toBe(string)
+    })
+
+    it("should show error prompt when a user click button without inputing a string", async () => {
+        const user = userEvent.setup()
+        render(<MemoryRouter><HomeScreen /></MemoryRouter>)
+
+        const button = screen.getByTestId("submitBtn") as HTMLButtonElement
+
+        await user.click(button)
+
+        const errorPrompt = screen.getByTestId("errorPrompt")
+
+        expect(errorPrompt).toBeInTheDocument()
+    })
+})
+
+describe("Integration Test", () => {
+    it("should route user to wordParseScreen after a user typed a string and click the submit button", async () => {
+        let string = "aabcaccda";
+        const user = userEvent.setup()
+        const routes = GetRoutes()
+
+        render(
+            <MemoryRouter initialIndex={0} initialEntries={["/", `/${string}`]}>
+                <Routes>
+                    {routes}
+                </Routes>
+            </MemoryRouter>
+        )
+
+        // user types
+        const inputField = screen.getByTestId("inputField") as HTMLInputElement;
+        await user.type(inputField, string);
+
+        // user clicks submit button
+        const button = screen.getByTestId("submitBtn") as HTMLButtonElement
+        await user.click(button)
+
+
+        const wordParserWrapper = await screen.findByTestId("wordParserWrapper")
+
+        expect(wordParserWrapper).toBeInTheDocument()
+
+    })
 })
