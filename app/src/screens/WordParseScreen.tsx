b424c5c4971796f2de06507d9d0516a325938551
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-03-18 09:56:50 +0100

    progress: implemented duplicate card remover animation gsap flip

--- app/src/screens/WordParseScreen.tsx
+++ app/src/screens/WordParseScreen.tsx
@@ -1,7 +1,11 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import React, { MouseEvent, TouchEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { ArrowLeftIcon, ArrowLongDownIcon, TrashIcon, ArrowLongRightIcon, CheckCircleIcon, XMarkIcon } from "@heroicons/react/24/solid"
 import { nanoid } from "nanoid"
+import gsap from "gsap"
+import { Flip } from "gsap/dist/Flip"
+
+gsap.registerPlugin(Flip);
 
 interface CharType {
 
@@ -14,7 +18,7 @@ interface CharType {
 
 function WordParseScreen() {
     const navigate = useNavigate()
-    const charStoreRef = useRef<Record<string, string>>({})
+    const charColorStoreRef = useRef<Record<string, string>>({})
     const sucessTextRef = useRef<HTMLParagraphElement | null>(null);
     const [userInput, setUserInput] = useState("");
     const [resultantString, setResultantString] = useState<string>("");
@@ -25,13 +29,73 @@ function WordParseScreen() {
         navigate(-1)
     }
 
-    const removeCharacter = (item: CharType) => {
+    const removeCharacter = (index: number, item: CharType) => {
         const newStr = resultantString.split("").filter((char, i) => {
             return item.char.toLowerCase() !== char.toLowerCase() || item.originalIndex === i
         }).join("")
 
-        setResultantString(newStr);
-        setShowSuccessOverlay(!getIsDuplicate(newStr))
+        const isDuplicate = getIsDuplicate(newStr)
+
+        setShowSuccessOverlay(!isDuplicate)
+
+        if (isDuplicate || newStr !== resultantString) {
+            const clickedCard = document.querySelector(`[data-cardindex="${index}"]`)
+            const cardsWrapper = document.querySelector("[data-cards-wrapper]") as HTMLDivElement
+            const cards = Array.from(cardsWrapper.children) as HTMLDivElement[]
+
+            cards.forEach((card, i) => {
+                const cardValue = card.getAttribute("data-card-charvalue")?.toLowerCase()
+                if (item.char.toLowerCase() === cardValue && clickedCard !== card) {
+                    card.style.filter = `url(#noise-${i})`
+                }
+            })
+
+            const state = Flip.getState(cards)
+
+
+            cards.forEach((card) => {
+                const cardValue = card.getAttribute("data-card-charvalue")?.toLowerCase()
+                if (item.char.toLowerCase() === cardValue && clickedCard !== card) {
+                    card.style.display = "none"
+                }
+            })
+
+
+            Flip.from(state, {
+                absolute: true,
+                duration: 1,
+                ease: "power1.inOut",
+                onLeave: (el) => {
+                    el.forEach(e => {
+                        const feTurbulance = e.querySelector(`feTurbulence`);
+                        const tl = gsap.timeline()
+                        tl.to(feTurbulance,
+                            {
+                                yoyo: true,
+                                yoyoEase: true,
+                                repeat: -1,
+                                attr: { baseFrequency: "0.05 0.07" }
+                            },
+                        ).to(
+                            el,
+                            {
+                                opacity: 0,
+                                scale: 0,
+                                duration: 2
+                            },
+                            "<"
+                        )
+                    })
+                },
+                onComplete: () => {
+                    setResultantString(newStr);
+                }
+            })
+        } else {
+            setResultantString(newStr);
+        }
+
+
     }
 
     const getIsDuplicate = useCallback((str: string) => {
@@ -51,6 +115,55 @@ function WordParseScreen() {
 
         return value;
     }, [])
+
+    const rotateCards = (e: MouseEvent, type: "enter" | "leave") => {
+        const el = e.currentTarget
+        const rect = el.getBoundingClientRect()
+
+        //  get mouse coord (e.g 0-1) relative to card element
+        let xCoord = (e.clientX - rect.left) / rect.width;
+        let yCoord = ((e.clientY - rect.top) / rect.height);
+
+        // rotate 3d values moving from -10,+10
+        let rotateX = (yCoord - 0.5) * 20
+        let rotateY = (xCoord - 0.5) * 20
+
+
+        if (type === "enter") {
+            gsap.to(
+                el,
+                {
+                    rotationX: rotateX,
+                    rotateZ: rotateY * 0.5,
+                    rotateY: rotateY,
+                    backgroundImage: `
+                    radial-gradient(farthest-corner circle at ${xCoord * 100}% ${yCoord * 100}% , white 20%, transparent 90%),
+                    url("/pattern.png"),
+                    url("/checkBoard.png")
+                    `,
+                    backgroundBlendMode: "soft-light,hue,soft-light",
+                    duration: 1
+                }
+            )
+
+        } else {
+            gsap.to(
+                el,
+                {
+                    rotationX: 0,
+                    rotateZ: 0,
+                    rotateY: 0,
+                    backgroundImage: `
+                    radial-gradient(farthest-corner circle at ${xCoord * 100}% ${yCoord * -100}% , transparent 5%, transparent 20%),
+                    url("/pattern.png"),
+                    url("/checkBoard.png")
+                    `,
+                    backgroundBlendMode: "soft-light,soft-light,hue",
+                    duration: 2
+                }
+            )
+        }
+    }
 
 
     const userInputCharacters = useMemo(() => {
@@ -79,18 +192,18 @@ function WordParseScreen() {
 
                 if (char !== " ") { // don't handle empty spaces
 
-                    if (charStoreRef.current[char.toLowerCase()]) {
+                    if (charColorStoreRef.current[char.toLowerCase()]) { // make sure identical char have the same background color
                         characters.push(
                             {
                                 char,
-                                color: charStoreRef.current[char.toLowerCase()],
+                                color: charColorStoreRef.current[char.toLowerCase()],
                                 originalIndex: i,
                                 id: nanoid()
                             }
                         )
                     } else {
                         const color = generateRandomColor();
-                        charStoreRef.current[char.toLowerCase()] = color;
+                        charColorStoreRef.current[char.toLowerCase()] = color;
                         characters.push(
                             {
                                 char,
@@ -115,7 +228,7 @@ function WordParseScreen() {
                     characters.push(
                         {
                             char,
-                            color: charStoreRef.current[char.toLowerCase()],
+                            color: charColorStoreRef.current[char.toLowerCase()],
                             originalIndex: i,
                             id: nanoid()
                         }
@@ -158,7 +271,7 @@ function WordParseScreen() {
 
     return (
         <div data-testid="wordParserWrapper" className='relative space-y-10 py-5'>
-            <div className='sticky bg-[#121212] p-4 rounded-md top-[75px] py-2 z-[2] flex flex-col items-center space-y-3 md:flex-row md:space-x-4 md:space-y-0'>
+            <div className='md:sticky bg-[#121212] p-4 rounded-md top-[75px] py-2 z-[2] flex flex-col items-center space-y-3 md:flex-row md:space-x-4 md:space-y-0'>
                 <ArrowLeftIcon onClick={navigateBack} className='block cursor-pointer self-start w-6 h-6 md:self-center text-primary' />
                 <fieldset className='border border-gray-300 p-4 py-2 rounded-md md:flex-1'>
                     <legend className='text-gray-300 text-xs px-2 mx-auto'>Original String</legend>
@@ -193,36 +306,62 @@ function WordParseScreen() {
 
             </div>
 
-            <div className='grid grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] gap-5 md:grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))]'>
+            <div data-cards-wrapper className='grid grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))]  gap-8 md:grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))]'>
                 {
                     userInputCharacters.map((item, i) => (
-                        <p
-                            data-charid={i}
+                        <div
                             key={item.id}
-                            className={`group relative rounded-md w-28 h-28 shadow-black cursor-pointer md:w-52 md:h-52`}
-                            style={{ backgroundColor: item.color }}
-                            onTouchStart={() => removeCharacter(item)}
-
+                            data-card
+                            data-cardindex={i}
+                            data-card-charvalue={item.char}
+                            className='[perspective:300px] '
                         >
-                            <span
-                                className='flex justify-center items-center h-full text-white text-[4rem] '>
-                                {item.char}
-                            </span>
-                            <span
-                                data-testid={`trashIcon-${i}`}
-                                className='absolute top-4 right-4 -translate-y-5 opacity-0 transition-all duration-700 group-hover:translate-y-0 group-hover:opacity-100'
-                                onClick={() => removeCharacter(item)}
-                            >
-                                <TrashIcon className='w-6 h-6 text-gray-200' />
-                            </span>
+                            <p
 
-                        </p>
+                                className={`group relative bg-[radial-gradient(farthest-corner_circle_at_0%_0%,_transparent_5%,_transparent_20%),url("/pattern.png"),url("/checkBoard.png")]  [background-blend-mode:soft-light,soft-light,hue] bg-contain rounded-md w-28 h-28 shadow-black md:w-52 md:h-52`}
+                                style={{ backgroundColor: item.color }}
+                                onTouchStart={(e: TouchEvent) => removeCharacter(i, item)}
+                                onMouseMove={(e: MouseEvent) => rotateCards(e, "enter")}
+                                onMouseLeave={(e: MouseEvent) => rotateCards(e, "leave")}
+
+                            >
+                                <span
+                                    className='flex justify-center items-center h-full text-white text-[4rem] '>
+                                    {item.char}
+                                </span>
+                                <span
+                                    data-testid={`trashIcon-${i}`}
+                                    className='absolute bg-red-200 p-2 rounded-md cursor-pointer top-4 right-4 -translate-y-5 opacity-0 transition-all duration-700 group-hover:translate-y-0 group-hover:opacity-100'
+                                    onClick={(e: MouseEvent) => removeCharacter(i, item)}
+                                >
+                                    <TrashIcon className='w-6 h-6 text-red-500' />
+                                </span>
+
+                            </p>
+                            <svg className='absolute top-0 left-0 pointer-events-none w-full h-full z-[2]'>
+                                <filter id={`noise-${i}`} x='0%' y='0%' width='100%' height='100%' >
+                                    <feTurbulence
+                                        baseFrequency='0.02 0.03'
+                                        result='NOISE'
+                                        numOctaves={1}
+                                        id='image-turbulence'
+                                    />
+                                    <feDisplacementMap
+                                        in='SourceGraphic'
+                                        in2='NOISE'
+                                        scale={50}
+                                        id='image-displacement'
+                                    />
+                                </filter>
+                            </svg>
+                        </div>
+
                     ))
                 }
-            </div>
+            </div >
 
 
-        </div>
+        </div >
     )
 }
 
